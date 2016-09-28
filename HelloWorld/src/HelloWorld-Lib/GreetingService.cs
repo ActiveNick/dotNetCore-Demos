fd3c96e28b0d4baeaa4f38d936e5015456d09b1f
Nick Landry <nlandry@activenick.net>
2016-09-28 15:42:25 -0700

    Added basic .NET Core library

---
+++ HelloWorld/src/HelloWorld-Lib/GreetingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Lib
+{
+    public class GreetingService
+    {
+        public GreetingService()
+        {
+            // Empty constructor
+        }
+
+        public string SayHello(string username)
+        {
+            return $"Hello {username}, welcome to .NET Core!";
+        }
+    }
+}
--- HelloWorld/src/HelloWorld/Program.cs
+++ HelloWorld/src/HelloWorld/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWorld_Lib;
 
 namespace HelloWorld
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             Console.Title = "Hello World on .NET Core";
@@ -14,11 +16,13 @@ namespace HelloWorld
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Type in your name and press ENTER:");
 
+            GreetingService gsvc = new GreetingService();
+
             Console.ForegroundColor = ConsoleColor.Gray;
             string username = Console.ReadLine();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"Hello {username}, welcome to .NET Core!");
+            Console.WriteLine(gsvc.SayHello(username));
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\nPress any key to exit.");
