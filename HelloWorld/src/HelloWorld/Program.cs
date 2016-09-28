9496c79879b9e3b2a2f71d89c59c7a8af4a7290f
Nick Landry <nlandry@activenick.net>
2016-09-28 12:49:06 -0700

    Initial code commit

---
+++ HelloWorld/src/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "Hello World on .NET Core";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Type in your name and press ENTER:");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            string username = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Hello {username}, welcome to .NET Core!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadLine();
+            Console.ResetColor();
+        }
+    }
+}
