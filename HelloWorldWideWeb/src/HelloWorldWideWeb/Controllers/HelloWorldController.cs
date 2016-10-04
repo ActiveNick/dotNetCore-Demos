4e289845946412a9f2090d86342470ad9c2e1a32
Nick Landry <nlandry@activenick.net>
2016-10-03 23:56:20 -0400

    Added HelloWorld Controller

---
+++ HelloWorldWideWeb/src/HelloWorldWideWeb/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            // This code uses HtmlEncoder.Default.Encode to protect the app from malicious input
+            // (namely JavaScript). It also uses Interpolated Strings.
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
